ac372b77bd15677a57eb8a223f7010991cfd9488
segundara <segundara@gmail.com>
2020-07-20 02:29:03 +0300

    updated the app js file

--- product-sql-react/src/App.js
+++ product-sql-react/src/App.js
@@ -8,6 +8,7 @@ import Footer from './components/Footer';
 import Homepage from './components/MainPage';
 import Details from './components/Details';
 import Checkout from './components/Checkout';
+import Backoffice from './components/Backoffice';
 
 class App extends React.Component {
   state = {
@@ -17,15 +18,24 @@ class App extends React.Component {
   addProductToCart = (count) => {
     this.setState({count})
   }
+  
+  addToCart = async(productid, userid) => {
+        const url = `http://localhost:3234/cart/${userid}`
+        const resp = await fetch(url)
+        if(resp.ok){
+          console.log(resp.json())
+        }
+  }
 
 
   render(){
     return (
         <Router>
           <Navbar updateProductInCart={this.state.count} />
-          <Route path="/" exact render={(props) => <Homepage {...props} addToCart={this.addProductToCart}  />} />
+          <Route path="/" exact render={(props) => <Homepage {...props} addToCart={this.addProductToCart} updateCartInfo={this.addToCart} />} />
           <Route path="/productDetails/:id" component={Details} />
           <Route path="/checkout" component={Checkout} />
+          <Route path="/backoffice" component={Backoffice} />
           {/* <Footer /> */}
         </Router>
     );
