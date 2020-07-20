140ce74b5414de24b12e1ff12f39e8596184f76c
segundara <segundara@gmail.com>
2020-07-20 09:05:46 +0300

    updated checkout component

--- product-sql-react/src/components/Checkout.jsx
+++ product-sql-react/src/components/Checkout.jsx
@@ -1,33 +1,66 @@
 import React from 'react'
-import { Alert,Button } from 'react-bootstrap'
+import { Alert,Button, Table, Container } from 'react-bootstrap'
 
 class Checkout extends React.Component {
     state={
-        cartTotal:0
+        cartTotal:0,
+        cartItems: []
     }
 
     componentDidMount = async () => {
-        const url = `http://localhost:3004/customers?name=Bee`
-        const user = await fetch(url)
-        if(user.ok){
-            const userJSON = await user.json()
-            console.log(userJSON.data[0]._id)
-            const url = `http://localhost:3004/customers/${userJSON.data[0]._id}/calculate-cart-total`
-            const cartTotal = await fetch(url)
-            if(cartTotal.ok){
-                const total = await cartTotal.json()
-                this.setState({cartTotal: total.total})
-                //console.log(total.total)
-            }
+        
+        const url = `http://localhost:3234/cart/1`
+        const resp = await fetch(url)
+        if(resp.ok){
+          const products = await resp.json()
+          const totalArray = products.map(item => parseFloat(item.total))
+          let totalAmount = totalArray.reduce((acc, curr) => {return (acc + curr)})
+          this.setState({cartItems: products,
+                        cartTotal: totalAmount
+                    })
+            console.log(totalArray)
         }
     }
 
     render(){
+        console.log(this.state.cartItems)
         return (
+            <Container>
             <div className="text-center mt-5">
+                <Table striped bordered hover size="sm">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>name</th>
+                            <th>unit price</th>
+                            <th>quantity</th>
+                            <th>total price</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                            {this.state.cartItems.map((item,i)=>(
+                                <tr key={i}>
+                                    <th>{i+1}</th>
+                                    <th>{item.name}</th>
+                                    <th>{item.unitary_price}</th>
+                                    <th>{item.quantity}</th>
+                                    <th>{item.total}</th>
+                                </tr>
+                            )
+                            )}
+                            <tr>
+                                <th></th>
+                                <th></th>
+                                <th></th>
+                                <th>Overall Total</th>
+                                <th>€{this.state.cartTotal}</th>
+                            </tr>
+                    </tbody>
+                </Table>
                 <Alert variant="info">Total purchase is €{this.state.cartTotal}</Alert>
                 <Button variant="success" onClick={() => this.makePayment()}>Make Payment</Button>
             </div>
+            </Container>
         )
     }
 }
