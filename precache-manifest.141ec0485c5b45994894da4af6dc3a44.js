f747380351b286427581d8133a72eec83229bea4
segundara <segundara@gmail.com>
2020-11-16 18:43:16 +0200

    Updates

---
+++ precache-manifest.141ec0485c5b45994894da4af6dc3a44.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23d8ac1f005f4fed93402b2740f2dc31",
+    "url": "/M6-sql-Benchmark-FE/index.html"
+  },
+  {
+    "revision": "76a90e7dbbc4ae7029fc",
+    "url": "/M6-sql-Benchmark-FE/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "f08d762c5c63b78de0ca",
+    "url": "/M6-sql-Benchmark-FE/static/css/main.0f79d55a.chunk.css"
+  },
+  {
+    "revision": "76a90e7dbbc4ae7029fc",
+    "url": "/M6-sql-Benchmark-FE/static/js/2.298cdcb3.chunk.js"
+  },
+  {
+    "revision": "434eeea35bf11e3d22931f972c8fbb4c",
+    "url": "/M6-sql-Benchmark-FE/static/js/2.298cdcb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f08d762c5c63b78de0ca",
+    "url": "/M6-sql-Benchmark-FE/static/js/main.e0864144.chunk.js"
+  },
+  {
+    "revision": "5a751088bcff4cb9bf8b",
+    "url": "/M6-sql-Benchmark-FE/static/js/runtime-main.ae5d6baa.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/M6-sql-Benchmark-FE/static/media/logo.5d5d9eef.svg"
+  }
+]);
